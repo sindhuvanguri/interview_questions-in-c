3cfe1298dd071e1dd0dd0e6714bdb78613d83711
sindhuvanguri <sindhuvanguri997@gmail.com>
2026-02-02 15:59:17 +0530

    Add files via upload

---
+++ sqpty.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void main()
+{
+int cigars;
+int weekend;
+printf("enter the value of cigars" );
+scanf("%d",&cigars);
+    if(weekend ==1)
+    {
+    if(cigars>=40)        
+    {
+        printf("true");
+    }
+    }
+    else
+    if(cigars>=40&&cigars<=60)    
+    {
+        printf("true");
+        
+    }
+    else
+    {
+        printf("false");
+    }   
+}
+
